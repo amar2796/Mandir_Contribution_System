ea9c6d5e888bd72f744e22d8e1b9404dd050b342
Amarjeet Vishwakarma <amarjeetvish@gmail.com>
2026-04-30 08:37:16 +0530

    dob issue incorrect value show

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzKaQzbEnH9yuJjjQJXxQDPE1pg4yPPSDgzGpR4Jkr0VrbKuQ6cwJQ7OLzuZsqB1ByY/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzruRDexulA_ZchsrPrCXw3RDBtsXDvtJDZiDq0nbt5Q7OdHG8m85QzB_Sh2G5xXrUU/exec";
